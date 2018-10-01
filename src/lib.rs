244cfba11f459fa8a6844c8191d3830eac2489e8
Christian Johansson <christian@cvj.se>
2018-10-01 07:21:27 +0200

    Improved usage documentation

--- src/lib.rs
+++ src/lib.rs
@@ -20,8 +20,19 @@
 //! ```rust,dont_run
 //! extern crate milstian_http;
 //! ```
-
-// TODO Code usage example here
+//!
+//! ## Example
+//! ```rust
+//! extern crate milstian_http;
+//! use milstian_http::request::{Message, Method, Protocol};
+//! 
+//! let response =
+//!     Message::from_tcp_stream(b"POST / HTTP/1.0\r\nAgent: Random browser\r\n\r\ntest=abc");
+//! assert!(response.is_some());
+//! let response_unwrapped = response.expect("POST HTTP1");
+//! assert_eq!(response_unwrapped.request_line.method, Method::Post);
+//! assert_eq!(response_unwrapped.request_line.protocol, Protocol::V1_0);
+//! ```
 
 pub mod request;
 pub mod response;
